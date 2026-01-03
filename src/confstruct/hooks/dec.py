0ff8466a1af9b2cac68a07d274af3dd97ce22c2c
equalred <amerfoe@gmail.com>
2026-01-03 21:11:27 +0300

    impr

--- src/confstruct/hooks/dec.py
+++ src/confstruct/hooks/dec.py
@@ -3,7 +3,7 @@ from typing import Any
 
 def dec_hook(typ: type, value: Any) -> Any:
     if hasattr(typ, "__validate__"):
-        val = typ.__validate__(value)
+        val = typ.__validate__(value, typ)
         if isinstance(val, typ):
             return val
         return typ(val)
--- src/confstruct/types/lists.py
+++ src/confstruct/types/lists.py
@@ -1,18 +1,12 @@
-from typing import Any
+from typing import Any, get_args
 
 
 class ListOf(list):
-    __item_type__: type
-
-    def __class_getitem__(cls, item_type: type):  # pyright: ignore[reportIncompatibleMethodOverride]
-        name = f"{cls.__name__}[{getattr(item_type, '__name__', str(item_type))}]"
-        return type(name, (cls,), {"__item_type__": item_type})
-
     @classmethod
-    def __validate__(cls, value: Any):
+    def __validate__(cls, value: Any, typ: type):
+        t = get_args(typ)[0]
         if isinstance(value, str):
             parts = [p.strip() for p in value.split(",") if p.strip()]
-            t = cls.__item_type__
             return cls([t(p) for p in parts])
         if isinstance(value, list):
             return cls(value)
