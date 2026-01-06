2ff2e190dec4fd9865960d286893d4ccca21f510
skarnu <amerfoe@gmail.com>
2026-01-06 17:23:35 +0300

    dotenv provider fix

--- src/confstruct/providers/dotenv_prov.py
+++ src/confstruct/providers/dotenv_prov.py
@@ -8,17 +8,18 @@ from .env_prov import EnvProvider
 class DotenvProvider(EnvProvider):
     """Provider that loads .env files."""
 
-    def __init__(self, dotenv_path: str | Path | None = None, override: bool = False) -> None:
+    def __init__(
+        self,
+        prefix: str = "",
+        case_sensitive: bool = False,
+        dotenv_path: str | Path | None = None,
+        override: bool = False,
+    ) -> None:
         """Initialize provider and load .env file.
 
         Args:
             dotenv_path: Path to .env file
             override: Override existing environment variables
         """
-        super().__init__()
-        if isinstance(dotenv_path, Path):
-            load_dotenv(dotenv_path=dotenv_path, override=override)
-        elif isinstance(dotenv_path, str):
-            load_dotenv(dotenv_path=Path(dotenv_path), override=override)
-        else:
-            load_dotenv(override=override)
+        load_dotenv(dotenv_path=dotenv_path, override=override)
+        super().__init__(prefix=prefix, case_sensitive=case_sensitive)
